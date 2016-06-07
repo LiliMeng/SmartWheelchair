c46392429924ffa18e0e1f83b11fe022a8654b74
LiliMeng <lilimeng1103@gmail.com>
2016-06-07 12:23:03 -0700

    Update joy_node.cpp

--- joy_node.cpp
+++ joy_node.cpp
@@ -213,7 +213,7 @@ public:
     pub_count_ = 0;
     lastDiagTime_ = ros::Time::now().toSec();
 
-    // Big while loop opens, publishes
+    // Big while loop opens, publishes my own recorded data
     while (nh_.ok())
     {
       open_ = false;
@@ -261,6 +261,8 @@ public:
       std::string data_loc="/home/lci/workspace/wheelchairTest/velocity1.txt";
       readData read_data(data_loc);
 
+
+      //
       while (nh_.ok())
       {
         ros::spinOnce();
@@ -346,7 +348,7 @@ public:
 
             for(int i=0; i<read_data.fb_joy.size(); i++)
             {
-                joy_msg.axes[3]= read_data.fb_joy[i];
+                joy_msg.axes[3]= -read_data.fb_joy[i];
                 std::cout<<read_data.fb_joy[i];
                 joy_msg.axes[2] = read_data.lr_joy[i];
 
@@ -396,6 +398,135 @@ public:
 
 
       } // End of joystick open loop.
+
+
+
+      while (nh_.ok())
+      {
+        ros::spinOnce();
+
+        bool publish_now = false;
+        bool publish_soon = false;
+        FD_ZERO(&set);
+        FD_SET(joy_fd, &set);
+
+        //ROS_INFO("Select...");
+        int select_out = select(joy_fd+1, &set, NULL, NULL, &tv);
+        //ROS_INFO("Tick...");
+        if (select_out == -1)
+        {
+          tv.tv_sec = 0;
+          tv.tv_usec = 0;
+          //ROS_INFO("Select returned negative. %i", ros::isShuttingDown());
+          continue;
+          //				break; // Joystick is probably closed. Not sure if this case is useful.
+        }
+
+        if (FD_ISSET(joy_fd, &set))
+        {
+          if (read(joy_fd, &event, sizeof(js_event)) == -1 && errno != EAGAIN)
+            break; // Joystick is probably closed. Definitely occurs.
+
+          //ROS_INFO("Read data...");
+          joy_msg.header.stamp = ros::Time().now();
+          event_count_++;
+          switch(event.type)
+          {
+          case JS_EVENT_BUTTON:
+          case JS_EVENT_BUTTON | JS_EVENT_INIT:
+            if(event.number >= joy_msg.buttons.size())
+            {
+              int old_size = joy_msg.buttons.size();
+              joy_msg.buttons.resize(event.number+1);
+              for(unsigned int i=old_size;i<joy_msg.buttons.size();i++)
+                joy_msg.buttons[i] = 0.0;
+            }
+            joy_msg.buttons[event.number] = (event.value ? 1 : 0);
+            // For initial events, wait a bit before sending to try to catch
+            // all the initial events.
+            if (!(event.type & JS_EVENT_INIT))
+              publish_now = true;
+            else
+              publish_soon = true;
+            break;
+          case JS_EVENT_AXIS:
+          case JS_EVENT_AXIS | JS_EVENT_INIT:
+            if(event.number >= joy_msg.axes.size())
+            {
+              int old_size = joy_msg.axes.size();
+              joy_msg.axes.resize(event.number+1);
+              for(unsigned int i=old_size;i<joy_msg.axes.size();i++)
+                joy_msg.axes[i] = 0.0;
+            }
+            if (!(event.type & JS_EVENT_INIT)) // Init event.value is wrong.
+            {
+              double val = event.value;
+              // Allows deadzone to be "smooth"
+              if (val > unscaled_deadzone)
+                val -= unscaled_deadzone;
+              else if (val < -unscaled_deadzone)
+                val += unscaled_deadzone;
+              else
+                val = 0;
+              joy_msg.axes[event.number] = val * scale;
+            }
+            // Will wait a bit before sending to try to combine events.
+            publish_soon = true;
+            break;
+          default:
+            ROS_WARN("joy_node: Unknown event type. Please file a ticket. time=%u, value=%d, type=%Xh, number=%d", event.time, event.value, event.type, event.number);
+            break;
+          }
+        }
+        else if (tv_set) // Assume that the timer has expired.
+          publish_now = true;
+
+        if (publish_now)
+        {
+
+                //Assume that all the JS_EVENT_INIT messages have arrived already.
+                //This should be the case as the kernel sends them along as soon as the device opens
+                //ROS_INFO("Publish...");
+                pub_.publish(joy_msg);
+                publish_now = false;
+                tv_set = false;
+                publication_pending = false;
+                publish_soon = false;
+                pub_count_++;
+
+        }
+
+        // If an axis event occurred, start a timer to combine with other
+        // events.
+        if (!publication_pending && publish_soon)
+        {
+          tv.tv_sec = trunc(coalesce_interval_);
+          tv.tv_usec = (coalesce_interval_ - tv.tv_sec) * 1e6;
+          publication_pending = true;
+          tv_set = true;
+          //ROS_INFO("Pub pending...");
+        }
+
+        // If nothing is going on, start a timer to do autorepeat.
+        if (!tv_set && autorepeat_rate_ > 0)
+        {
+          tv.tv_sec = trunc(autorepeat_interval);
+          tv.tv_usec = (autorepeat_interval - tv.tv_sec) * 1e6;
+          tv_set = true;
+          //ROS_INFO("Autorepeat pending... %i %i", tv.tv_sec, tv.tv_usec);
+        }
+
+        if (!tv_set)
+        {
+          tv.tv_sec = 1;
+          tv.tv_usec = 0;
+        }
+
+        diagnostic_.update();
+
+
+      } // End of joystick open loop.
+
 
       close(joy_fd);
       ros::spinOnce();
